da0f296a5ebf95edd40f9604740b101e2addec19
aion0721 <rudolf.aion@gmail.com>
2023-10-11 17:34:23 +0900

    add axios post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handlePrompt = async () => {
+    try {
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: "Javascriptを教えてください。" }],
+        }),
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setResponse(response.data.choices[0].message.content);
+    } catch (error) {
+      console.error(`Error ChatGPT: ${error}`);
+    }
+  };
   return (
     <>
       <input
@@ -10,10 +33,11 @@ function App() {
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
       ></input>
-      <button onClick={() => console.log(prompt)}>prompt</button>
+      <button onClick={() => handlePrompt()}>prompt</button>
       <button onClick={() => console.log(import.meta.env.VITE_OPENAI_API_KEY)}>
         api
       </button>
+      {response}
     </>
   );
 }
