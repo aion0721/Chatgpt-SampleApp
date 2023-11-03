cb8f8eede577360b0758448a119e65fb487de40d
aion0721 <rudolf.aion@gmail.com>
2023-11-03 14:47:34 +0900

    Create Loading Dots

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import axios from "axios";
+import LoadingDots from "./LoadingDots";
 
 function App() {
   const [prompt, setPrompt] = useState("");
-  const [response, setResponse] = useState("");
+  const [response, setResponse] = useState("Answer Area");
 
   const handlePrompt = async () => {
     try {
-      setResponse("Loading...");
+      setResponse("Loading");
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
         JSON.stringify({
@@ -28,19 +29,45 @@ function App() {
       console.error(`Error ChatGPT: ${error}`);
     }
   };
+  const setResponseSample = () => {
+    setResponse(
+      "JavaScriptは、ウェブページの動的な要素やインタラクティブな機能を実現するために使用されるプログラミング言語です。\nHTMLやCSSと組み合わせて使用され、クライアントサイドで実行されます。JavaScriptは、ユーザーの入力に応じてページを変更したり、データを取得して表示したり、動画やアニメーションなどのマルチメディア要素を操作したりすることができます。\nまた、最近では、サーバーサイドやモバイルアプリケーションの開発にも使用されることがあります。"
+    );
+  };
   return (
     <>
-      <input
-        type="text"
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-      ></input>
-      <button onClick={() => handlePrompt()}>prompt</button>
-      <button onClick={() => console.log(import.meta.env.VITE_OPENAI_API_KEY)}>
-        api
-      </button>
-      <button onClick={() => console.log(response)}>response</button>
-      {response}
+      <div className="chat-container">
+        <header className="chat-header">
+          <h1>ChatGPT App</h1>
+        </header>
+
+        <section className="chat-message"></section>
+
+        <div className="message-form">
+          <input
+            type="text"
+            value={prompt}
+            className="message-input"
+            placeholder="Please input prompt..."
+            onChange={(e) => setPrompt(e.target.value)}
+          ></input>
+          <button onClick={() => handlePrompt()} className="send-button">
+            prompt
+          </button>
+        </div>
+        <div className="message gpt-response">
+          <div className="text">
+            {response === "Loading" ? <LoadingDots /> : response}
+          </div>
+        </div>
+        <button
+          onClick={() => console.log(import.meta.env.VITE_OPENAI_API_KEY)}
+        >
+          api
+        </button>
+        <button onClick={() => console.log(response)}>response</button>
+        <button onClick={setResponseSample}>set</button>
+      </div>
     </>
   );
 }
---
+++ src/LoadingDots.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+const LoadingDots = () => {
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // 現在のドットの状態に基づいて次の状態を設定
+      setDots((prevDots) => (prevDots.length < 3 ? prevDots + "." : ""));
+    }, 1000); // 1000ミリ秒ごとに更新
+
+    // コンポーネントのアンマウント時にインターバルをクリア
+    return () => clearInterval(intervalId);
+  }, []); // 空の依存配列でマウント時にのみ実行
+
+  return <>Loading{dots}</>;
+};
+
+export default LoadingDots;
